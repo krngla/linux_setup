d04c8752e14ae17da7cfc3510bc68988fb886242
arjevn <are.jevne@kitron.com>
2023-06-29 12:04:59 +0200

    added some comments

--- package_self.sh
+++ package_self.sh
@@ -11,17 +11,20 @@ fi
 cat << 'EOF' > ../output/installscripts.sh
 #!/bin/bash
 
-echo $(basename $PWD)
-
+#Find start of payload
 PAYLOAD_LINE=$(awk '/^__PAYLOAD_BEGINS__/ { print NR + 1; exit 0; }' $0)
 
+#Find top layer of tarball
 dir_name=$(tail -n +${PAYLOAD_LINE} $0 | tar -zpt | head -1 | cut -f1 -d"/")
 tail -n +${PAYLOAD_LINE} $0 | tar -zpvx
+
 echo $dir_name
 echo Starting install process...
 cd $dir_name
 ./install.sh -u $1
 cd ..
+
+#Cleanup pwd
 rm -rf $dir_name
 exit 0
 __PAYLOAD_BEGINS__ 
